8c85a1c67a8994fda85507a68a0b7191a764145a
Joseph Oguntebi <joseph.oguntebi@yahoo.com>
2021-07-30 23:21:07 +0100

    initial login logout button setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import LoginButton from './Components/LoginButton';
+import LogoutButton from './Components/LogoutButton';
+// import Profile from './Profile'
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <LoginButton />
+      <LogoutButton />
+    </>
+  );
+}
+
+export default App;
