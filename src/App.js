98a2104c50a24d92b33f966f6ed0c05c67790c43
Joseph Oguntebi <joseph.oguntebi@yahoo.com>
2021-08-01 02:23:16 +0100

    isAuthenticated feature for Login and Logout Buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import React from 'react'
 import LoginButton from './Components/LoginButton';
 import LogoutButton from './Components/LogoutButton';
-// import Profile from './Profile'
+import Profile from './Profile'
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react' 
 
 function App() {
+  const { isLoading } = useAuth0;
+  if (isLoading) return <div>Loading...</div>
+  
   return (
     <>
       <LoginButton />
       <LogoutButton />
+      <Profile />
     </>
   );
 }
